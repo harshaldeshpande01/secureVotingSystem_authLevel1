7fa82acf8dd7ef54865eda991400966a94e03eda
Harshal Deshpande <harshal25012001@gmail.com>
2021-09-04 18:17:45 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try{
+    await mongoose.connect(process.env.DATABASE_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    });
+    console.log("MongoDB Connected");
+  }
+  catch(err)
+ {
+   console.log(err);
+ }
+};
+
+module.exports = connectDB;
